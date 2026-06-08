be19c1a75ffce9adf9719718d286782b08a589ca
Hemnixx <123105111@nitkkr.ac.in>
2026-06-09 03:16:47 +0530

    error fixed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://bloodbank-git-main-hemnixxs-projects.vercel.app", "http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
